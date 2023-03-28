ddf5bd1eb299ebedbc3ade8901df6a558ab5cd13
NarathipDev <narathip_aot@hotmail.com>
2023-03-29 00:29:53 +0700

    Update CalendarScreen to connection in firestore

--- scheduleme_application/lib/screens/Calendar/calendar.dart
+++ scheduleme_application/lib/screens/Calendar/calendar.dart
@@ -1,8 +1,12 @@
 import 'dart:convert';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_connect/http/src/utils/utils.dart';
 import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
@@ -15,70 +19,169 @@ class CalendarScreen extends StatefulWidget {
 }
 
 class _CalendarScreenState extends State<CalendarScreen> {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final FirebaseFirestore fireStore = FirebaseFirestore.instance;
+  final CollectionReference _profileCollection =
+      FirebaseFirestore.instance.collection("Profile");
+
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDate;
 
   Map<String, List> mySelectedEvents = {};
+  var currentInfo = [];
 
   final titleController = TextEditingController();
   final descriptionController = TextEditingController();
+
+  final String day = DateTime.now().day.toString();
+  final int numDay = DateTime.now().weekday;
+  final int numMonth = DateTime.now().month;
+  final int years = DateTime.now().year;
+
+  Future<void> _getCurrentUserData() async {
+    final currentUser = auth.currentUser!;
+    if (currentUser != Null) {
+      final collectionReference = fireStore.collection("Profile");
+      final documentReference = await collectionReference.get();
+      documentReference.docs.forEach((element) {
+        if (element.get("uid") == auth.currentUser!.uid) {
+          currentInfo = [
+            element.get("full_name").toString(),
+            element.get("id").toString(),
+            element.get("faculty").toString(),
+            element.get("major").toString(),
+            element.get("uid").toString(),
+            element.get("chatID").toString(),
+            element.get("email").toString(),
+          ];
+        }
+      });
+    }
+  }
+
+  void _loadPreviousEvents() async {
+    try {
+      final doc = await FirebaseFirestore.instance
+          .collection('Profile')
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .get();
+      if (doc.exists) {
+        final eventsMap = Map<String, dynamic>.from(doc.data()!['events'] as Map);
+        setState(() {
+          mySelectedEvents = eventsMap.cast<String, List<dynamic>>();
+        });
+      }
+    } catch (e) {
+      print('Error loading previous events: $e');
+    }
+  }
 
   @override
   void initState() {
     super.initState();
     _selectedDate = _focusedDay;
-    loadPreviousEvents();
+    _getCurrentUserData();
+    _loadPreviousEvents();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            TableCalendar(
-              firstDay: DateTime(2022),
-              lastDay: DateTime(2025),
-              focusedDay: _focusedDay,
-              calendarFormat: _calendarFormat,
-              onDaySelected: (selectedDay, focusedDay) {
-                if (!isSameDay(_selectedDate, selectedDay)) {
-                  setState(() {
-                    _selectedDate = selectedDay;
-                    _focusedDay = focusedDay;
-                  });
-                }
-              },
-              selectedDayPredicate: (day) {
-                return isSameDay(_selectedDate, day);
-              },
-              onFormatChanged: (format) {
-                if (_calendarFormat != format) {
-                  setState(() {
-                    _calendarFormat = format;
-                  });
-                }
-              },
-              onPageChanged: (focusedDay) {
-                _focusedDay = focusedDay;
-              },
-              eventLoader: _listOfDayEvents,
-            ),
-            ..._listOfDayEvents(_selectedDate!).map(
-              (myEvents) => ListTile(
-                leading: const Icon(
-                  Icons.adjust,
-                  color: Colors.teal,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 50),
+                      child: Text(
+                        day,
+                        style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: ListTile(
+                        title: Text(_showWeekDay(numDay)),
+                        subtitle: Text(_showMonth(numMonth) + " " + years.toString()),
+                      ),
+                    )
+                  ],
                 ),
-                title: Padding(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text("Event: " + myEvents['eventTitle']),
-                ),
-                subtitle: Text("Description: " + myEvents['description']),
               ),
-            )
-          ],
+              TableCalendar(
+                firstDay: DateTime(2022),
+                lastDay: DateTime(2025),
+                focusedDay: _focusedDay,
+                calendarFormat: _calendarFormat,
+                onDaySelected: (selectedDay, focusedDay) {
+                  if (!isSameDay(_selectedDate, selectedDay)) {
+                    setState(() {
+                      _selectedDate = selectedDay;
+                      _focusedDay = focusedDay;
+                    });
+                  }
+                },
+                selectedDayPredicate: (day) {
+                  return isSameDay(_selectedDate, day);
+                },
+                onFormatChanged: (format) {
+                  if (_calendarFormat != format) {
+                    setState(() {
+                      _calendarFormat = format;
+                    });
+                  }
+                },
+                onPageChanged: (focusedDay) {
+                  _focusedDay = focusedDay;
+                },
+                eventLoader: _listOfDayEvents,
+              ),
+              ..._listOfDayEvents(_selectedDate!).map(
+                (myEvents) => Padding(
+                  padding:
+                      const EdgeInsets.only(left: 20.0, right: 20, bottom: 5, top: 20),
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Color(0xffC5BDBD),
+                        width: 2,
+                      ),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: ListTile(
+                      leading: const Icon(
+                        Icons.adjust,
+                        color: Colors.teal,
+                      ),
+                      title: Padding(
+                        padding: const EdgeInsets.only(bottom: 8),
+                        child: Text(
+                          "Event: " + myEvents['eventTitle'],
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                      subtitle: Text(
+                        "Description: " + myEvents['description'],
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -90,6 +193,10 @@ class _CalendarScreenState extends State<CalendarScreen> {
   }
 
   _showAddEventDialog() async {
+    final TextEditingController titleController = TextEditingController();
+    final TextEditingController descriptionController = TextEditingController();
+    DateTime? selectedDate;
+
     await showDialog(
       context: context,
       builder: (context) => AlertDialog(
@@ -100,63 +207,81 @@ class _CalendarScreenState extends State<CalendarScreen> {
         content: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisSize: MainAxisSize.min,
-          children: [
+          children: <Widget>[
             TextField(
               controller: titleController,
-              textCapitalization: TextCapitalization.words,
               decoration: const InputDecoration(
-                label: Text("Event"),
+                hintText: "Event Title",
               ),
             ),
             TextField(
               controller: descriptionController,
-              textCapitalization: TextCapitalization.words,
               decoration: const InputDecoration(
-                label: Text("Description"),
+                hintText: "Description",
               ),
             ),
           ],
         ),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: const Text("Cancel"),
-          ),
+        actions: <Widget>[
           TextButton(
             onPressed: () {
-              if (titleController.text.isEmpty && descriptionController.text.isEmpty) {
+              Navigator.of(context).pop();
+            },
+            child: const Text(
+              "Cancel",
+              style: TextStyle(
+                color: Colors.red,
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () async {
+              final eventTitle = titleController.text.trim();
+              final description = descriptionController.text.trim();
+
+              if (eventTitle.isEmpty || description.isEmpty) {
                 ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
                   content: Text("Required Event and Description."),
                   duration: Duration(seconds: 2),
                 ));
                 return;
-              } else {
-                setState(() {
-                  if (mySelectedEvents[DateFormat('yyyy-MM-dd').format(_selectedDate!)] !=
-                      null) {
-                    mySelectedEvents[DateFormat('yyyy-MM-dd').format(_selectedDate!)]!
-                        .add({
-                      "eventTitle": titleController.text,
-                      "description": descriptionController.text
-                    });
-                  } else {
-                    mySelectedEvents[DateFormat('yyyy-MM-dd').format(_selectedDate!)] = [
-                      {
-                        "eventTitle": titleController.text,
-                        "description": descriptionController.text
-                      }
-                    ];
-                  }
-                });
-
-                titleController.clear();
-                descriptionController.clear();
-                Navigator.pop(context);
-                return;
               }
+
+              final String formattedDate =
+                  DateFormat('yyyy-MM-dd').format(_selectedDate!);
+              if (mySelectedEvents[formattedDate] != null) {
+                mySelectedEvents[formattedDate]!.add({
+                  "eventTitle": eventTitle,
+                  "description": description,
+                });
+              } else {
+                mySelectedEvents[formattedDate] = [
+                  {
+                    "eventTitle": eventTitle,
+                    "description": description,
+                  }
+                ];
+              }
+
+              try {
+                await _profileCollection.doc(auth.currentUser!.uid).update({
+                  "events":
+                      mySelectedEvents.map((key, value) => MapEntry(key, value.toList())),
+                });
+                setState(() {});
+              } catch (e) {
+                print(e);
+              }
+
+              Navigator.of(context).pop();
             },
-            child: const Text("Add Event"),
-          )
+            child: const Text(
+              "Save",
+              style: TextStyle(
+                color: Colors.green,
+              ),
+            ),
+          ),
         ],
       ),
     );
@@ -170,5 +295,34 @@ class _CalendarScreenState extends State<CalendarScreen> {
     }
   }
 
-  loadPreviousEvents() {}
+  String _showWeekDay(int numDay) {
+    Map<int, String> weekday = {
+      1: "Monday",
+      2: "Tuesday",
+      3: "Wednesday",
+      4: "Thursday",
+      5: "Friday",
+      6: "Saturday",
+      7: "Sunday"
+    };
+    return weekday[numDay].toString();
+  }
+
+  String _showMonth(int numMonth) {
+    Map<int, String> months = {
+      1: "January",
+      2: "February",
+      3: "March",
+      4: "April",
+      5: "May",
+      6: "June",
+      7: "July",
+      8: "August",
+      9: "September",
+      10: "October",
+      11: "November",
+      12: "December"
+    };
+    return months[numMonth].toString();
+  }
 }
